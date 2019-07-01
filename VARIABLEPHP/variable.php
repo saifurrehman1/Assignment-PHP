1b044c3ddf1b085a44b646a4283cbb7ae800dfdc
Mian Saif <50007819+saifurrehman1@users.noreply.github.com>
2019-07-01 22:33:03 +0500

    Add files via upload

---
+++ VARIABLEPHP/variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+	<?php 
+
+	$txt = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+
+	?>
+
+</body>
+</html>
